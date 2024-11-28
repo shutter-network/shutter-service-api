639725afca9f4dff96895680a11626c29ec3d421
faheelsattar <faheelsattar@yahoo.com>
2024-11-29 02:27:22 +0500

    add error middleware

---
+++ internal/middleware/error.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/shutter-network/shutter-service-api/internal/error"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ErrorHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Next()
+		for _, err := range ctx.Errors {
+			switch e := err.Err.(type) {
+			case *error.Http:
+				ctx.AbortWithStatusJSON(e.StatusCode, e)
+			case error.Http:
+				ctx.AbortWithStatusJSON(e.StatusCode, e)
+			default:
+				ctx.AbortWithStatusJSON(http.StatusInternalServerError, map[string]string{"message": "Service Unavailable"})
+			}
+		}
+	}
+}
