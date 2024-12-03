a29b004df1f31470bae0f82b264d574f09886982
faheelsattar <faheelsattar@yahoo.com>
2024-12-03 22:24:05 +0500

    expose svc

--- internal/service/crypto.go
+++ internal/service/crypto.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"net/http"
-	"strconv"
 
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/gin-gonic/gin"
@@ -12,6 +11,11 @@ import (
 	"github.com/shutter-network/shutter-service-api/internal/error"
 	"github.com/shutter-network/shutter-service-api/internal/usecase"
 )
+
+type RegisterIdentityRequest struct {
+	DecryptionTimestamp uint64 `json:"decryptionTimestamp"`
+	IdentityPrefix      string `json:"identityPrefix"`
+}
 
 type CryptoService struct {
 	CryptoUsecase *usecase.CryptoUsecase
@@ -78,34 +82,19 @@ func (svc *CryptoService) GetDataForEncryption(ctx *gin.Context) {
 }
 
 func (svc *CryptoService) RegisterIdentity(ctx *gin.Context) {
-	decryptionTimestampStringified, ok := ctx.GetQuery("decryptionTimestamp")
-	if !ok {
+	var req RegisterIdentityRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		log.Err(err).Msg("err decoding request body")
 		err := error.NewHttpError(
-			"query parameter not found",
-			"decryptionTimestamp query parameter is required",
+			"unable to decode request body",
+			"",
 			http.StatusBadRequest,
 		)
 		ctx.Error(err)
 		return
 	}
 
-	decryptionTimestamp, err := strconv.Atoi(decryptionTimestampStringified)
-	if err != nil {
-		log.Err(err).Msg("err decoding decryptionTimestamp")
-		err := error.NewHttpError(
-			"unable to decode decryptionTimestamp",
-			"valid decryptionTimestamp query parameter is required",
-			http.StatusBadRequest,
-		)
-		ctx.Error(err)
-		return
-	}
-	identityPrefix, ok := ctx.GetQuery("identityPrefix")
-	if !ok {
-		identityPrefix = ""
-	}
-
-	data, httpErr := svc.CryptoUsecase.RegisterIdentity(ctx, uint64(decryptionTimestamp), identityPrefix)
+	data, httpErr := svc.CryptoUsecase.RegisterIdentity(ctx, req.DecryptionTimestamp, req.IdentityPrefix)
 	if httpErr != nil {
 		ctx.Error(httpErr)
 		return
