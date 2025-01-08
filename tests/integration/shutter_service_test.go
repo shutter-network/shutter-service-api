e35e8667c81e962ff7e0986af733a372c471a9be
faheelsattar <faheelsattar@yahoo.com>
2025-01-08 21:43:03 +0500

    add test

--- tests/integration/shutter_service_test.go
+++ tests/integration/shutter_service_test.go
@@ -1,37 +1,87 @@
 package integration
 
 import (
+	"bytes"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"net/http/httptest"
+	"time"
+
+	httpError "github.com/shutter-network/shutter-service-api/internal/error"
 
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/shutter-network/shutter-service-api/internal/service"
 	"github.com/shutter-network/shutter-service-api/internal/usecase"
-	"github.com/stretchr/testify/assert"
 )
 
-func (s *TestShutterService) TestGetDataForEncryption() {
+var msg = []byte("please hide this message")
+
+func (s *TestShutterService) TestRequestDecryptionKeyBeforeTimestampReached() {
 	address := crypto.PubkeyToAddress(*s.config.PublicKey).Hex()
 	identityPrefix, err := generateRandomBytes(32)
 	s.Require().NoError(err)
 	identityPrefixStringified := hex.EncodeToString(identityPrefix)
 
 	query := fmt.Sprintf("?address=%s&identityPrefix=%s", address, identityPrefixStringified)
-	url := s.testServer.URL + "/api/get_data_for_encryption" + query
+	url := "/api/get_data_for_encryption" + query
 
-	resp, err := http.Get(url)
-	assert.NoError(s.T(), err)
-	defer resp.Body.Close()
+	recorder := httptest.NewRecorder()
+	s.router.ServeHTTP(recorder, httptest.NewRequest("GET", url, nil))
 
-	s.Require().Equal(http.StatusOK, resp.StatusCode)
+	s.Require().Equal(http.StatusOK, recorder.Code)
 
-	var response usecase.GetDataForEncryptionResponse
-	err = json.NewDecoder(resp.Body).Decode(&response)
-
+	body, err := io.ReadAll(recorder.Body)
 	s.Require().NoError(err)
-	s.Require().Greater(response.Eon, uint64(1))
-	s.Require().NotNil(response.EonKey)
-	s.Require().NotNil(response.Identity)
-	s.Require().NotNil(response.IdentityPrefix)
+
+	var dataForEncryptionResponse map[string]usecase.GetDataForEncryptionResponse
+	err = json.Unmarshal(body, &dataForEncryptionResponse)
+	s.Require().NoError(err)
+
+	res := dataForEncryptionResponse["message"]
+	s.Require().Greater(res.Eon, uint64(1))
+	s.Require().NotNil(res.EonKey)
+	s.Require().NotNil(res.Identity)
+	s.Require().NotNil(res.IdentityPrefix)
+
+	identityStringified := res.Identity
+
+	decryptionTimestamp := time.Now().Add(1 * time.Hour).Unix()
+	reqBody := service.RegisterIdentityRequest{
+		DecryptionTimestamp: uint64(decryptionTimestamp),
+		IdentityPrefix:      identityPrefixStringified,
+	}
+
+	jsonData, err := json.Marshal(reqBody)
+	s.Require().NoError(err)
+	url = "/api/register_identity"
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	s.Require().NoError(err)
+
+	req.Header.Set("Content-Type", "application/json")
+
+	recorder = httptest.NewRecorder()
+
+	s.router.ServeHTTP(recorder, req)
+	s.Require().Equal(http.StatusOK, recorder.Code)
+
+	query = fmt.Sprintf("?identity=%s", identityStringified)
+	url = "/api/get_decryption_key" + query
+
+	recorder = httptest.NewRecorder()
+	s.router.ServeHTTP(recorder, httptest.NewRequest("GET", url, nil))
+
+	s.Require().Equal(http.StatusBadRequest, recorder.Code)
+
+	body, err = io.ReadAll(recorder.Body)
+	s.Require().NoError(err)
+
+	var errorResponse httpError.Http
+	err = json.Unmarshal(body, &errorResponse)
+	s.Require().NoError(err)
+
+	s.Require().Equal(errorResponse.Description, "timestamp not reached yet, decryption key requested too early")
 }
