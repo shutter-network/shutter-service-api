aac3cdeba5aabcbed2ce81930042fe68e0648c8f
faheelsattar <faheelsattar@yahoo.com>
2025-01-16 17:39:10 +0500

    add test

--- tests/integration/shutter_service_test.go
+++ tests/integration/shutter_service_test.go
@@ -339,3 +339,67 @@ func (s *TestShutterService) TestRequestDecryptCommitmentAfterTimestampReached()
 	s.Require().NotEmpty(decryptedMessage)
 	s.Require().Equal(msg, decryptedMessage)
 }
+
+func (s *TestShutterService) TestRegisterIdentityInThePast() {
+	if testing.Short() {
+		s.T().Skip("skipping integration test")
+	}
+	ctx := context.Background()
+	address := crypto.PubkeyToAddress(*s.config.PublicKey).Hex()
+	identityPrefix, err := generateRandomBytes(32)
+	s.Require().NoError(err)
+	identityPrefixStringified := hex.EncodeToString(identityPrefix)
+
+	query := fmt.Sprintf("?address=%s&identityPrefix=%s", address, identityPrefixStringified)
+	url := "/api/get_data_for_encryption" + query
+
+	recorder := httptest.NewRecorder()
+	s.router.ServeHTTP(recorder, httptest.NewRequest("GET", url, nil))
+
+	s.Require().Equal(http.StatusOK, recorder.Code)
+
+	body, err := io.ReadAll(recorder.Body)
+	s.Require().NoError(err)
+
+	var dataForEncryptionResponse map[string]usecase.GetDataForEncryptionResponse
+	err = json.Unmarshal(body, &dataForEncryptionResponse)
+	s.Require().NoError(err)
+
+	res := dataForEncryptionResponse["message"]
+	s.Require().GreaterOrEqual(res.Eon, uint64(1))
+	s.Require().NotNil(res.EonKey)
+	s.Require().NotNil(res.Identity)
+	s.Require().NotNil(res.IdentityPrefix)
+
+	block, err := s.ethClient.BlockByNumber(ctx, nil)
+	s.Require().NoError(err)
+
+	decryptionTimestamp := block.Header().Time - 10
+	reqBody := service.RegisterIdentityRequest{
+		DecryptionTimestamp: uint64(decryptionTimestamp),
+		IdentityPrefix:      identityPrefixStringified,
+	}
+
+	jsonData, err := json.Marshal(reqBody)
+	s.Require().NoError(err)
+	url = "/api/register_identity"
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	s.Require().NoError(err)
+
+	req.Header.Set("Content-Type", "application/json")
+
+	recorder = httptest.NewRecorder()
+
+	s.router.ServeHTTP(recorder, req)
+	s.Require().Equal(http.StatusBadRequest, recorder.Code)
+
+	body, err = io.ReadAll(recorder.Body)
+	s.Require().NoError(err)
+
+	var errorResponse httpError.Http
+	err = json.Unmarshal(body, &errorResponse)
+	s.Require().NoError(err)
+
+	s.Require().Equal(errorResponse.Description, "decryption timestamp should be in future")
+}
